f808d6dd7a3a3186468f3344a1f79724c2eca754
Alif Abdul Rauf <2011522024_alif@student.unand.ac.id>
2022-10-22 00:32:12 +0700

    loginn

--- app/src/main/java/com/example/hackathonn/ui/Login.kt
+++ app/src/main/java/com/example/hackathonn/ui/Login.kt
@@ -1,12 +1,25 @@
 package com.example.hackathonn.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.hackathonn.MainActivity
 import com.example.hackathonn.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val btnlogin = findViewById<Button>(R.id.btn_login)
+
+        btnlogin.setOnClickListener(){
+            Intent(this, MainActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+
+
     }
 }
